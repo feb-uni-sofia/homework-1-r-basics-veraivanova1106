8bd5caabc548271bd4c7f6292b4e5282079e657b
veraivanova1106 <37923114+veraivanova1106@users.noreply.github.com>
2018-03-30 17:24:51 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,20 @@
-
+#a)  Create a vector with the numbers 4, 1, 1, 4 called x
+x<- c(4,1,1,4)
+#b)  Create a second vector called y with the numbers 1, 4
+y<- c(1,4)
+#c)The two vectors have different sizes so the shorter one(y) gets repeated so it turns into (1,4,1,4)
+x-y
+#d)
+s<- c(x,y)
+#e)
+rep(s,10)
+z<-rep(s,10)
+length(z)
+#f)
+rep(s, each=3)
+#g)
+seq(7,21,by=1)
+lseq<-seq(7,21,by=1)
+7:21
+#h)
+length(lseq)
